ac6cd292d6bac96514a0512d2fef06d8b9e33bd0
Artur Moczulski <artur@fuseignited.com>
2022-12-26 15:22:18 -0800

    test: add a mock http server

--- test/book.js
+++ test/book.js
@@ -1,12 +1,27 @@
 const assert = require('assert');
+const nock = require('nock')
+const request = require('supertest')("https://the-one-api.dev/v2")
+const expect = require('chai').expect
 
 module.exports = (theOneSDK) => {
   return () => {
 
     describe('#books()', function () {
-      it('should return the list of LOTR books', function () {
-        theOneSDK.books()
-        // code here
+      it('should return the list of LOTR books', async function () {
+
+        nock("https://the-one-api.dev/v2/")
+          .get("/book/")
+          .replyWithFile(200, __dirname + '/mocks/allBooks.json', {
+            'Content-Type': 'application/json',
+          })
+
+        const response = await request
+          .get('/book/')
+
+        expect(response.status).equal(200);
+        expect(response.body.total).equal(3);
+
+        // theOneSDK.books()
       });
     });
 
