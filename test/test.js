4c874a0a50ca1c228c13922b98ff49a96c2fba64
Artur Moczulski <artur@fuseignited.com>
2022-12-26 13:08:34 -0800

    test: init the testing framework

---
+++ test/test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+describe('theOneSDK', function () {
+  describe('#books()', function () {
+    it('should return the list of LOTR books', function () {
+      // code here
+    });
+  });
+});
