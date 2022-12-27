a42e8f5accab63b93a3b2eca90bc6a152a51e6f5
Artur Moczulski <artur@fuseignited.com>
2022-12-26 16:34:57 -0800

    feat: support init without options

--- test/test.js
+++ test/test.js
@@ -1,8 +1,37 @@
 const assert = require('assert');
+const { expect } = require('chai');
 
-const theOneSDK = require('../lib/entry')({ debug: "DEBUG" })
-const bookSuite = require('./book')(theOneSDK)
 
 describe('theOneSDK', function () {
-  describe('book suite', bookSuite.bind(this));
+
+  describe('initialization', () => {
+    describe('with no SDK options', () => {
+      const theOneSDK = require('../lib/entry')()
+
+      it('bulids the SDK object correctly', () => {
+        expect(theOneSDK.books).to.be.a("function")
+      });
+      
+    });
+
+    describe('with SDK options', () => {
+      const theOneSDK = require('../lib/entry')({debug: "DEBUG"})
+
+      it('bulids the SDK object correctly', () => {
+        expect(theOneSDK.books).to.be.a("function")
+      });
+      
+    });
+  });
+  
+
+  describe('suites', () => {
+
+    const theOneSDK = require('../lib/entry')({debug: "DEBUG"})
+
+    const bookSuite = require('./book')(theOneSDK)
+
+    describe('book suite', bookSuite.bind(this));
+  });
+  
 });
