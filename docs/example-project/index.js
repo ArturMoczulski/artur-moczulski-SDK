dd98318ad34377fbcdea7c12553569c3926e197a
Artur Moczulski <artur@fuseignited.com>
2022-12-26 17:46:40 -0800

    docs: add listing chapters to the example app

--- docs/example-project/index.js
+++ docs/example-project/index.js
@@ -5,6 +5,7 @@ term.cyan( 'The One API browser\n' ) ;
 
 var items = [
 	'a. What LOTR books have been written?' ,
+	'b. What are the chapters of the first LOTR book?' ,
 ] ;
 
 term.singleColumnMenu( items , async function( error , response ) {
@@ -12,7 +13,18 @@ term.singleColumnMenu( items , async function( error , response ) {
   let responseText;
 
   if (response.selectedIndex == 0) {
+
     let books = await theOneSDK.books();
+
+    responseText = books.map(book => book.name)
+
+  } else if (response.selectedIndex == 1) {
+
+    let books = await theOneSDK.books();
+    let fellowship = books[0]
+    await fellowship.fetchChapters()
+    responseText = fellowship.chapters.map(chapter => chapter.chapterName)
+
     responseText = books.map(book => book.name)
   }
 
