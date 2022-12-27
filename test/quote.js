2c3bf5e061814d4a9146d9583e525c8228c7881e
Artur Moczulski <artur@fuseignited.com>
2022-12-26 18:37:14 -0800

    feat: use chai as promised to handle auth errors testing

--- test/quote.js
+++ test/quote.js
@@ -1,27 +1,51 @@
 const assert = require('assert');
 const nock = require('nock')
 const request = require('supertest')("https://the-one-api.dev/v2")
-const expect = require('chai').expect
+const chai = require('chai')
+const chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised)
+const expect = chai.expect
 
 module.exports = (theOneSDK) => {
   return () => {
 
     describe('#quotes()', function () {
-      this.beforeAll(() => {
-        nock("https://the-one-api.dev/v2/", {
-          reqheaders: {
-            "Authorization": 'Bearer 123',
-          },
-        })
-          .get("/quote/")
-          .replyWithFile(200, __dirname + '/mocks/allQuotes.json', {
-            'Content-Type': 'application/json',
+      describe('with correct API access key', () => {
+        this.beforeAll(() => {
+          nock("https://the-one-api.dev/v2/", {
+            reqheaders: {
+              "Authorization": 'Bearer 123',
+            },
           })
-      })
+            .get("/quote/")
+            .replyWithFile(200, __dirname + '/mocks/allQuotes.json', {
+              'Content-Type': 'application/json',
+            })
+        })
 
-      it('should return the list of LOTR books quotes', async function () {
-        const result = await theOneSDK.quotes()
-        expect(result.length).equal(1000)
+        it('should return the list of LOTR books quotes', async function () {
+          const result = await theOneSDK.quotes()
+          expect(result.length).equal(1000)
+        });
+      });
+      
+      describe('with incorrect API access key', () => {
+        this.beforeAll(() => {
+          nock("https://the-one-api.dev/v2/", {
+            reqheaders: {
+              "Authorization": 'Bearer 123',
+            },
+          })
+            .get("/quote/")
+            .reply(401, {
+              "success": false,
+              "message": "Unauthorized."
+            })
+        })
+
+        it('should return authentication error', async function () {
+          await expect(theOneSDK.quotes()).to.be.rejectedWith(Error, "Unauthorized.")
+        });
       });
     });
 
