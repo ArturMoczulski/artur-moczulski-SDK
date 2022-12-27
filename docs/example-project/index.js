2011a74489d8f090d11c5f16c5abd7d1e90744d9
Artur Moczulski <artur@fuseignited.com>
2022-12-26 16:11:48 -0800

    docs: add books browsing to the exapmle project

--- docs/example-project/index.js
+++ docs/example-project/index.js
@@ -1,1 +1,25 @@
-const theOneSDK = require('artur-moczulski-sdk')({ debug: "DEBUG" })
+const theOneSDK = require('artur-moczulski-sdk')
+const term = require( 'terminal-kit' ).terminal ;
+
+term.cyan( 'The One API browser\n' ) ;
+
+var items = [
+	'a. What LOTR books have been written?' ,
+] ;
+
+term.singleColumnMenu( items , async function( error , response ) {
+
+  let responseText;
+
+  if (response.selectedIndex == 0) {
+    let books = await theOneSDK.books();
+    responseText = books.map(book => book.name)
+  }
+
+	term( '\n' ).eraseLineAfter.green(
+		responseText+"\n" 
+	) ;
+
+	process.exit() ;
+
+} ) ;
